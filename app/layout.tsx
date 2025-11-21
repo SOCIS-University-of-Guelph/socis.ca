688c2246e6c85fdd8f57822c8812f52732e0d1a4
Syed Maroof <maroof@example.com>
2025-11-20 22:10:47 -0500

    basic fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-mainblack h-screen">
 
         <NavigationBar />
+        
         <main>
 
           {children}
